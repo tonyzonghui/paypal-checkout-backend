0303d94b944d9af53be19458e9f02c4dd813cee3
Tony Zonghui <tonyzhang@silots.com>
2020-06-28 21:03:28 +0800

    [TASK] Finished updating payments using order APIs

---
+++ api/commons/PaypalClient.js
@@ -0,0 +1,57 @@
+const checkoutNodeJssdk = require("@paypal/checkout-server-sdk");
+
+/**
+ *
+ * Returns PayPal HTTP client instance with environment that has access
+ * credentials context. Use this instance to invoke PayPal APIs, provided the
+ * credentials have access.
+ */
+function client() {
+  return new checkoutNodeJssdk.core.PayPalHttpClient(environment());
+}
+
+/**
+ *
+ * Returns PayPal HTTP client instance with environment that has access
+ * credentials context. Use this instance to invoke PayPal APIs, provided the
+ * credentials have access.
+ */
+function client() {
+  return new checkoutNodeJssdk.core.PayPalHttpClient(environment());
+}
+
+/**
+ *
+ * Set up and return PayPal JavaScript SDK environment with PayPal access credentials.
+ * This sample uses SandboxEnvironment. In production, use LiveEnvironment.
+ *
+ */
+function environment() {
+  let clientId = sails.config.paypal.clientId || "PAYPAL-SANDBOX-CLIENT-ID";
+  let clientSecret =
+    sails.config.paypal.clientSecret || "PAYPAL-SANDBOX-CLIENT-SECRET";
+
+  return new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
+}
+
+async function prettyPrint(jsonData, pre = "") {
+  let pretty = "";
+  function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  }
+  for (let key in jsonData) {
+    if (jsonData.hasOwnProperty(key)) {
+      if (isNaN(key)) pretty += pre + capitalize(key) + ": ";
+      else pretty += pre + (parseInt(key) + 1) + ": ";
+      if (typeof jsonData[key] === "object") {
+        pretty += "\n";
+        pretty += await prettyPrint(jsonData[key], pre + "    ");
+      } else {
+        pretty += jsonData[key] + "\n";
+      }
+    }
+  }
+  return pretty;
+}
+
+module.exports = { client: client, prettyPrint: prettyPrint };
--- api/services/PaypalService.js
+++ api/services/PaypalService.js
@@ -1,132 +1,54 @@
-const paypal = require("paypal-rest-sdk");
-const Payment = require("../models/Payment");
 const axios = require("axios");
-
-paypal.configure({
-  mode: sails.config.paypal.mode,
-  client_id: sails.config.paypal.clientId,
-  client_secret: sails.config.paypal.clientSecret,
-});
+const paypal = require("@paypal/checkout-server-sdk");
+const paypalClient = require("../commons/PaypalClient");
 
 module.exports = {
   createPayment(amount, currency, callback) {
-    let paymentJson = {
-      intent: "sale",
-      payer: {
-        payment_method: "paypal",
-      },
-      transactions: [
+    const request = new paypal.orders.OrdersCreateRequest();
+    request.prefer("return=representation");
+    request.requestBody({
+      intent: "CAPTURE",
+      purchase_units: [
         {
           amount: {
-            total: amount,
-            currency: currency,
+            currency_code: currency,
+            value: amount,
           },
         },
       ],
-      redirect_urls: {
-        return_url: "http://localhost:8080",
-        cancel_url: "http://localhost:8080",
-      },
-    };
-    console.log(JSON.stringify(paymentJson));
+    });
+    paypalClient
+      .client()
+      .execute(request)
+      .then((order) => {
+        console.log("order: ", order);
 
-    this.callPaypalCreatePaymentAPI(paymentJson)
-      .then((result) => {
-        console.log("create payment result: ", response);
-        return callback(null, { id: response.id });
+        return callback(null, { orderID: order.result.id });
       })
       .catch((error) => {
-        console.log("create payment error: ", JSON.stringify(error));
+        console.log("error: ", error);
+
         return callback(error);
       });
-    // paypal.payment.create(paymentJson, (error, response) => {
-    //   if (error) {
-    //     console.log("create payment error: ", JSON.stringify(error));
-    //     return callback(error);
-    //   } else {
-    //     console.log("create payment result: ", response);
-    //     return callback(null, { id: response.id });
-    //   }
-    // });
   },
 
-  executePayment(paymentID, paymentJson, payment, callback) {
-    this.callPaypalExecutePaymentAPI(paymentID, paymentJson)
-      .then((paymentLog) => {
-        console.log("payment success: ", paymentLog);
-        payment.email = paymentLog.payer.payer_info.email;
-        payment.first_name = paymentLog.payer.payer_info.first_name;
-        payment.last_name = paymentLog.payer.payer_info.last_name;
-        console.log(payment);
+  captureOrderPayment(orderID, callback) {
+    const request = new paypal.orders.OrdersCaptureRequest(orderID);
+    request.requestBody({});
+    paypalClient
+      .client()
+      .execute(request)
+      .then((capture) => {
+        console.log("capture: ", JSON.stringify(capture));
+        let payment = {};
+        payment.email = capture.result.payer.email_address;
+        payment.first_name = capture.result.payer.name.given_name;
+        payment.last_name = capture.result.payer.name.surname;
         callback(null, payment);
       })
       .catch((error) => {
         console.log("error: ", error);
-        return callback(error);
+        callback(error);
       });
-    // paypal.payment.execute(paymentID, paymentJson, (error, paymentLog) => {
-    //   if (error) {
-    //     console.log("error: ", error);
-
-    //     return callback(error);
-    //   } else {
-    //     console.log("payment success: ", paymentLog);
-
-    //     payment.email = paymentLog.payer.payer_info.email;
-    //     payment.first_name = paymentLog.payer.payer_info.first_name;
-    //     payment.last_name = paymentLog.payer.payer_info.last_name;
-    //     console.log(payment);
-    //     callback(null, payment);
-    //     // Payment.create(payment).exec((err, result) => {
-    //     //   callback(null, "done");
-    //     // });
-    //   }
-    // });
-  },
-
-  callPaypalCreatePaymentAPI(paymentJson) {
-    return new Promise((resolve, reject) => {
-      axios
-        .post(
-          sails.config.paypal.apiBaseUrl + "/v1/payments/payment",
-          this.constructPaypalRequestJson(paymentJson)
-        )
-        .then((result) => {
-          return resolve(result);
-        })
-        .catch((error) => {
-          return reject(error);
-        });
-    });
-  },
-
-  callPaypalExecutePaymentAPI(paymentID, paymentJson) {
-    return new Promise((resolve, reject) => {
-      axios
-        .post(
-          sails.config.paypal.apiBaseUrl +
-            "/v1/payments/payment" +
-            paymentID +
-            "/execute",
-          this.constructPaypalRequestJson(paymentJson)
-        )
-        .then((result) => {
-          return resolve(result);
-        })
-        .catch((error) => {
-          return reject(error);
-        });
-    });
-  },
-
-  constructPaypalRequestJson(bodyJson) {
-    return {
-      auth: {
-        user: sails.config.paypal.clientId,
-        pass: sails.config.paypal.clientSecret,
-      },
-      body: bodyJson,
-      json: true,
-    };
   },
 };
